7008fb1a99c30d1ed8efe1e99fcda7b024ea71f2
Philip Engberg <philipengberg@gmail.com>
2016-02-27 12:23:00 +0100

    Basic background fetching + local notifications working

--- PendlerGo/AppDelegate.swift
+++ PendlerGo/AppDelegate.swift
@@ -10,11 +10,14 @@ import UIKit
 import Google
 import Fabric
 import Crashlytics
+import RxSwift
+import RxCocoa
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let bag = DisposeBag()
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
@@ -36,7 +39,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         GGLContext.sharedInstance().configureWithError(&configureError)
         assert(configureError == nil, "Error configuring Google services: \(configureError)")
         
+        UIApplication.sharedApplication().setMinimumBackgroundFetchInterval(
+            UIApplicationBackgroundFetchIntervalMinimum)
+        
+        UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Alert, .Sound], categories: nil))
+        
         return true
+    }
+    
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        
+        
+//        UIApplication.sharedApplication().cancelAllLocalNotifications()
+//        let notification = UILocalNotification()
+//        notification.alertBody = "Tjek om dit tog er forsinket"
+//        notification.fireDate = NSDate(year: 2016, month: 2, day: 26, hour: 21, minute: 1, second: 0)
+//        notification.soundName = UILocalNotificationDefaultSoundName
+//        notification.applicationIconBadgeNumber = 5
+////        notification.repeatInterval = .Minute
+//        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+//        
+        for lol in UIApplication.sharedApplication().scheduledLocalNotifications! {
+            print(lol)
+        }
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -55,14 +80,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         
-        Settings.sharedSettings.initialize()
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+//        Settings.sharedSettings.initialize()
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(application: UIApplication, performFetchWithCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        
+        PendlerGoAPI.request(.Board(locationId: Settings.sharedSettings.workLocation!.id)).mapJSON().mapToObject(DepartureBoard).map({ (board) -> [Departure] in
+            return board.departures
+        }).subscribeNext { (departures) -> Void in
+            
+            var delayed = ""
+            var track = ""
+            for departure in departures {
+                if !departure.realTime.isEmpty && departure.time != departure.realTime {
+                    delayed += "\(departure.name) kl. \(departure.time) er \(abs(Int(departure.realDepartureTime.minutesFrom(departure.departureTime)))) forsinket"
+                }
+                
+                if let realTrack = departure.realTrack where  realTrack != departure.track {
+                    if !track.isEmpty { track += "\n" }
+                    track += "\(departure.name) kl. \(departure.time) er ændret til Spor \(realTrack)"
+                }
+            }
+            
+            if !track.isEmpty {
+                UIApplication.sharedApplication().cancelAllLocalNotifications()
+                let notification = UILocalNotification()
+                notification.alertBody = track
+                notification.fireDate = NSDate()
+                notification.soundName = UILocalNotificationDefaultSoundName
+                notification.applicationIconBadgeNumber = 5
+                //        notification.repeatInterval = .Minute
+                UIApplication.sharedApplication().scheduleLocalNotification(notification)
+            }
+        }.addDisposableTo(bag)
+        
+        Settings.sharedSettings.homeLocationVariable.asObservable().take(1).subscribeNext({ (_) -> Void in
+            
+            
+            completionHandler(.NewData)
+        }).addDisposableTo(bag)
+        
+        Settings.sharedSettings.initialize()
+        
+    }
 
 }
 
